9387053a431e9eae54fb68f995b09595bef57dfc
mjcarnaje <michaeljamescarnaje1@gmail.com>
2020-07-24 07:02:14 +0800

    render the results to the ui, clearfields, clearresult

--- src/js/index.js
+++ src/js/index.js
@@ -12,19 +12,20 @@ const state = {
 const controlSearch = async () => {
 	// 1) Get query from view
 	const query = searchView.getInput();
-	console.log(query);
 
 	if (query) {
 		// 2) New search object and add to state
 		state.search = new Search(query);
 
 		// 3) Prepare UI for the results
+		searchView.clearInput();
+		searchView.clearResults();
 
 		// 4) Search for Recipes
 		await state.search.getResults();
 
 		// 5) Render results on IU
-		console.log(state.search.result);
+		searchView.renderResults(state.search.result);
 	}
 };
 
